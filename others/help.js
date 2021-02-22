921a0f85637b431e25d2bd0cec8f1f0a373e4d85
dev-calli721 <77979700+dev-calli721@users.noreply.github.com>
2021-02-22 15:25:00 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -22,30 +22,29 @@ module.exports = {
 
 
 
-<a:emoji_7:798080205138821170> **Command Music** <a:emoji_7:798080205138821170> 
+<a:emoji_5:797912346759659591> **Command Music** <a:emoji_5:797912346759659591> 
 
 <a:emoji_29:813102989723172906> \`m!filter\`        <a:emoji_29:813102989723172906> \`m!loop\`
 
 <a:emoji_29:813102989723172906> \`m!lyrics\`        <a:emoji_29:813102989723172906> \`m!nowplaing\`
 
-<a:emoji_29:813102989723172906> \`m!pause\`        <a:emoji_29:813102989723172906> \`m!play\`
+ <a:emoji_29:813102989723172906> \`m!pause\`         <a:emoji_29:813102989723172906> \`m!play\`
 
-<a:emoji_29:813102989723172906> \`m!queue\`        <a:emoji_29:813102989723172906> \`m!radio\`
+ <a:emoji_29:813102989723172906> \`m!queue\`         <a:emoji_29:813102989723172906> \`m!radio\`
 
-<a:emoji_29:813102989723172906> \`m!remove\`      <a:emoji_29:813102989723172906> \`m!resume\`
+<a:emoji_29:813102989723172906> \`m!remove\`        <a:emoji_29:813102989723172906> \`m!resume\`
 
-<a:emoji_29:813102989723172906> \`m!search\`      <a:emoji_29:813102989723172906> \`m!shuffle\`
+<a:emoji_29:813102989723172906> \`m!search\`        <a:emoji_29:813102989723172906> \`m!shuffle\`
 
-<a:emoji_29:813102989723172906> \`m!skip\`      <a:emoji_29:813102989723172906> \`m!skipto\`
+  <a:emoji_29:813102989723172906> \`m!skip\`          <a:emoji_29:813102989723172906> \`m!skipto\`
 
-<a:emoji_29:813102989723172906> \`m!stop\`      <a:emoji_29:813102989723172906> \`m!volume\`
+  <a:emoji_29:813102989723172906> \`m!stop\`          <a:emoji_29:813102989723172906> \`m!volume\`
 
 <a:emoji_5:797912346759659591> **Others** <a:emoji_5:797912346759659591>
 
 <a:emoji_29:813102989723172906> \`m!help\`        <a:emoji_29:813102989723172906> \`m!ping\`
 
 <a:emoji_29:813102989723172906> \`m!prefix\`      <a:emoji_29:813102989723172906> \`m!uptime\`
-
 
 [July Support Server](https://discord.gg/rWQh4SYQsR) - [July Invited](https://discord.com/api/oauth2/authorize?client_id=793214259932561419&permissions=8&scope=bot)
 
