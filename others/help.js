9d3a3718e9a537efc7864a578df35b3bc0fe1451
dev-calli721 <77979700+dev-calli721@users.noreply.github.com>
2021-02-22 14:46:53 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -6,145 +6,68 @@ const {
 } = require(`../config.json`);
 const db = require('quick.db');
 
+
 module.exports = {
-  name: `help`,
-  description: "(h)Gives you a list of all help Commands",
-  aliases: ["h","commands"],
-  cooldown: 3,
-  edesc: "Type help to get a short preview of all Commands, Type help <COMMANDNAME> to get extended information about this one command!",
-  async execute(message,args,client) {
-
-    let prefix = await db.get(`prefix_${message.guild.id}`)
-    if(prefix === null) prefix = PREFIX;
-    //react with approve emoji
-    message.react("769665713124016128");
-    //define the commands as a command
+  name: "help",
+  aliases: ["h"],
+  cooldown: 8,
+  description: "**all commands**",
+  execute(message) {
     let commands = message.client.commands.array();
-    //define the help embed
+
     let helpEmbed = new MessageEmbed()
-      .setTitle(`List of all commands`)
-      .setDescription("__**```List of all commands```**__")
-       .setImage('')
-       .setFooter(message.author.username, message.author.displayAvatarURL)
-       .setDescription(`**[  SUPPORT  ](https://discord.gg/jcs4XwcExv)**   -  [   INVITE   ](https://discord.com/api/oauth2/authorize?client_id=807519129736839178&permissions=70346817&scope=bot)`)
-      .setThumbnail(message.author.avatarURL)
-      .setColor('RANDOM');
-    //define argstrue to negative
-    let ifargstruedothis = -1;
+    .setTitle(`<a:22CE943240F04B89B202818358B89BD7:800236414171611173>  __**Prefix Bot +**__ <a:emoji_16:798111136145997824> `)
+    .setThumbnail(message.author.avatarURL())
+    .setDescription(`
 
-      switch(args[0]){
-          case "filter":
-           ifargstruedothis=0;
-          break;
-          case "loop":
-            ifargstruedothis=1;
-          break;
-          case "lyrics":
-            ifargstruedothis=2
-          break;
-          case "nowplaying":
-            ifargstruedothis=3
-          break;
-          case "pause":
-            ifargstruedothis=4
-          break;
-          case "play":
-            ifargstruedothis=5
-          break;
-          case "playlist":
-            ifargstruedothis=6
-          break;
-          case "queue":
-            ifargstruedothis=7
-          break;
-          case "radio":
-            ifargstruedothis=8
-          break;
-          case "remove":
-            ifargstruedothis=9
-          break;
-          case "resume":
-            ifargstruedothis=10
-          break;
-          case "search":
-            ifargstruedothis=11
-          break;
-          case "shuffle":
-            ifargstruedothis=12
-          break;
-          case "skip":
-            ifargstruedothis=13
-          break;
-          case "skipto":
-            ifargstruedothis=14
-          break;
-          case "stop":
-            ifargstruedothis=15
-          break;
-          case "volume":
-            ifargstruedothis=16
-          break;
-          case "botlist":
-            ifargstruedothis=17
-          break;
-          case "help":
-            ifargstruedothis=18
-          break;
-          case "invite":
-            ifargstruedothis=19
-          break;
-          case "ping":
-            ifargstruedothis=20
-          break;
-          case "prefix":
-            ifargstruedothis=21
-          break;
-          case "uptime":
-            ifargstruedothis=22
-          break;
-          default:
-            commands.forEach((cmd) => {
-              helpEmbed.addField(
-                `**${prefix}${cmd.name}**`,
-                `${cmd.description}`,
-                true
-              );
-            });
-          if(!message.guild) {
-            if(!args[0]) {message.react(approveemoji);return message.channel.send(helpEmbed);}
-            return
-            }
-            message.react(approveemoji);
-message.channel.send(helpEmbed)
+<a:emoji_7:798080205138821170>  __**Music Commands**__ <a:emoji_7:798080205138821170>
 
-        break;
-       }
+<a:emoji_29:813102989723172906> \`play\`
+<a:emoji_45:801530072560828417> 
+<a:emoji_29:813102989723172906> \`clip\`
+<a:emoji_45:801530072560828417> 
+<a:emoji_29:813102989723172906> \`clips\`
+<a:emoji_45:801530072560828417> 
+<a:emoji_29:813102989723172906>  \`loop\`
+<a:emoji_45:801530072560828417> 
+<a:emoji_29:813102989723172906> \`lyrics\`
+<a:emoji_45:801530072560828417> 
+<a:emoji_29:813102989723172906> \`pause\`
+<a:emoji_45:801530072560828417> 
+<a:emoji_29:813102989723172906> \`playlist\`
+<a:emoji_45:801530072560828417> 
+<a:emoji_29:813102989723172906> \`pruning\`
+<a:emoji_45:801530072560828417> 
+<a:emoji_29:813102989723172906> \`queue\`
+<a:emoji_45:801530072560828417> 
+<a:emoji_29:813102989723172906> \`remove\`
+<a:emoji_45:801530072560828417> 
+<a:emoji_29:813102989723172906> \`resume\`
+<a:emoji_45:801530072560828417> 
+<a:emoji_29:813102989723172906>  \`search\`
+<a:emoji_45:801530072560828417> 
+<a:emoji_29:813102989723172906>  \`shuffle\`
+<a:emoji_45:801530072560828417> 
+<a:emoji_29:813102989723172906>  \`skip\`
+<a:emoji_45:801530072560828417> 
+<a:emoji_29:813102989723172906> \`skipto\`
+<a:emoji_45:801530072560828417> 
+<a:emoji_29:813102989723172906> \`stop\`
+<a:emoji_45:801530072560828417> 
+<a:emoji_29:813102989723172906> \`uptime\`
+<a:emoji_45:801530072560828417> 
+<a:emoji_29:813102989723172906> \`volume\`
 
-       if(ifargstruedothis>=0){
-         let aliases = commands[ifargstruedothis].aliases;
-         if(aliases === undefined || !aliases) aliases="No Aliases!";
-         let cooldown = commands[ifargstruedothis].cooldown;
-         if(cooldown === undefined || !cooldown) cooldown="No Cooldown!";
+[July Support Server](https://discord.gg/rWQh4SYQsR) - [July Invited](https://discord.com/api/oauth2/authorize?client_id=793214259932561419&permissions=8&scope=bot)
 
 
-        helpEmbed.addField(
-          `**${prefix}${commands[ifargstruedothis].name}**`,
-          `\`\`\`fix\n${commands[ifargstruedothis].edesc}\n\`\`\`\n\`${commands[ifargstruedothis].description}\``
-        );
-        helpEmbed.addField(
-          `**:notes: Aliases:**`,
-          `\`${aliases}\``
-        );
-        helpEmbed.addField(
-          `**:wrench: Cooldown:**`,
-          `\`${cooldown}\``
-        );
-        if(!message.guild) return message.author.send(helpEmbed);
-          message.author.send(helpEmbed)
-          message.channel.send( new MessageEmbed().setColor("#c219d8")
-        .setDescription(`**👍 ${message.author} Check your \`direct messages\` for a list of Commands!**`)
-          );
-       }
+`)
 
-}
-}
+  
+   .setColor("BLUE");
+   helpEmbed.setTimestamp();
+   message.react("<a:emoji_29:813102989723172906>")
+    return message.channel.send(helpEmbed).catch(console.error);
+
+  }
+};
