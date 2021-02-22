861db80487b169eeb5dab1af1775d679b4bb7a15
dev-calli721 <77979700+dev-calli721@users.noreply.github.com>
2021-02-22 15:18:57 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -16,47 +16,36 @@ module.exports = {
     let commands = message.client.commands.array();
 
     let helpEmbed = new MessageEmbed()
-    .setTitle(`<a:22CE943240F04B89B202818358B89BD7:800236414171611173>  __**Prefix Bot +**__ <a:emoji_16:798111136145997824> `)
+    .setTitle(`<a:emoji_15:798111117053526017> **Prefix Bot m!** <a:emoji_16:798111136145997824>`)
     .setThumbnail(message.author.avatarURL())
     .setDescription(`
 
-<a:emoji_7:798080205138821170>  __**Music Commands**__ <a:emoji_7:798080205138821170>
 
-<a:emoji_29:813102989723172906> \`play\`
-<a:emoji_45:801530072560828417> 
-<a:emoji_29:813102989723172906> \`clip\`
-<a:emoji_45:801530072560828417> 
-<a:emoji_29:813102989723172906> \`clips\`
-<a:emoji_45:801530072560828417> 
-<a:emoji_29:813102989723172906>  \`loop\`
-<a:emoji_45:801530072560828417> 
-<a:emoji_29:813102989723172906> \`lyrics\`
-<a:emoji_45:801530072560828417> 
-<a:emoji_29:813102989723172906> \`pause\`
-<a:emoji_45:801530072560828417> 
-<a:emoji_29:813102989723172906> \`playlist\`
-<a:emoji_45:801530072560828417> 
-<a:emoji_29:813102989723172906> \`pruning\`
-<a:emoji_45:801530072560828417> 
-<a:emoji_29:813102989723172906> \`queue\`
-<a:emoji_45:801530072560828417> 
-<a:emoji_29:813102989723172906> \`remove\`
-<a:emoji_45:801530072560828417> 
-<a:emoji_29:813102989723172906> \`resume\`
-<a:emoji_45:801530072560828417> 
-<a:emoji_29:813102989723172906>  \`search\`
-<a:emoji_45:801530072560828417> 
-<a:emoji_29:813102989723172906>  \`shuffle\`
-<a:emoji_45:801530072560828417> 
-<a:emoji_29:813102989723172906>  \`skip\`
-<a:emoji_45:801530072560828417> 
-<a:emoji_29:813102989723172906> \`skipto\`
-<a:emoji_45:801530072560828417> 
-<a:emoji_29:813102989723172906> \`stop\`
-<a:emoji_45:801530072560828417> 
-<a:emoji_29:813102989723172906> \`uptime\`
-<a:emoji_45:801530072560828417> 
-<a:emoji_29:813102989723172906> \`volume\`
+
+<a:emoji_7:798080205138821170> **Command Music** <a:emoji_7:798080205138821170> 
+
+<a:emoji_29:813102989723172906> \`m!filter\`        <a:emoji_29:813102989723172906> \`m!loop\`
+
+<a:emoji_29:813102989723172906> \`m!lyrics\`        <a:emoji_29:813102989723172906> \`m!nowplaing\`
+
+<a:emoji_29:813102989723172906> \`m!pause\`        <a:emoji_29:813102989723172906> \`m!play\`
+
+<a:emoji_29:813102989723172906> \`m!queue\`        <a:emoji_29:813102989723172906> \`m!radio\`
+
+<a:emoji_29:813102989723172906> \`m!remove\`      <a:emoji_29:813102989723172906> \`m!resume\`
+
+<a:emoji_29:813102989723172906> \`m!search\`      <a:emoji_29:813102989723172906> \`m!shuffle\`
+
+<a:emoji_29:813102989723172906> \`m!skip\`      <a:emoji_29:813102989723172906> \`m!skipto\`
+
+<a:emoji_29:813102989723172906> \`m!stop\`      <a:emoji_29:813102989723172906> \`m!volume\`
+
+<a:emoji_5:797912346759659591> **Others** <a:emoji_5:797912346759659591>
+
+<a:emoji_29:813102989723172906> \`m!help\`        <a:emoji_29:813102989723172906> \`m!ping\`
+
+<a:emoji_29:813102989723172906> \`m!prefix\`      <a:emoji_29:813102989723172906> \`m!uptime\`
+
 
 [July Support Server](https://discord.gg/rWQh4SYQsR) - [July Invited](https://discord.com/api/oauth2/authorize?client_id=793214259932561419&permissions=8&scope=bot)
 
