03870cda8bf9a3c04d9ef376a1750543ef9b2e25
farhang427 <80125940+farhang427@users.noreply.github.com>
2021-03-09 17:12:10 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -19,44 +19,44 @@ module.exports = {
     .setThumbnail(message.author.avatarURL())
     .setDescription(`
 
- ≪  Prefix Bot m!  ≫
+ ≪  Prefix Bot @  ≫
 
-      <a:emoji_1:797796538939867137> command music <a:emoji_1:797796538939867137> 
+      <a:emoji_17:818843627621974056> command music <a:emoji_17:818843627621974056> 
 
- <a:emoji_30:812376416409747578> m!loop 
- <a:emoji_30:812376416409747578> m!lyrics              
- <a:emoji_30:812376416409747578> m!pause
- <a:emoji_30:812376416409747578> m!play                   
- <a:emoji_30:812376416409747578> m!queue
- <a:emoji_30:812376416409747578> m!radio                 
- <a:emoji_30:812376416409747578> m!remove
- <a:emoji_30:812376416409747578> m!resume             
- <a:emoji_30:812376416409747578> m!search
- <a:emoji_30:812376416409747578> m!shuffle              
- <a:emoji_30:812376416409747578> m!skip
- <a:emoji_30:812376416409747578> m!skipto               
-<a:emoji_30:812376416409747578>  m!stop
- <a:emoji_30:812376416409747578> m!volume
-
-      <a:emoji_5:797912346759659591> Filter Commands <a:emoji_5:797912346759659591>
-<a:emoji_9:797912443628027955>  m!fi 8D
- <a:emoji_9:797912443628027955> m!fi tremolo
- <a:emoji_9:797912443628027955> m!fi vibrato
- <a:emoji_9:797912443628027955> m!fi pulsator
- <a:emoji_9:797912443628027955> m!fi clear
- <a:emoji_9:797912443628027955> m!fi bassboost
- <a:emoji_9:797912443628027955> m!fi vaporwave
-
-      <a:emoji_7:797912407309287434> Others <a:emoji_7:797912407309287434> 
- <a:emoji_29:812376279814635591> m!help              <a:emoji_29:812376279814635591> m!ping
-<a:emoji_29:812376279814635591> m!prefix            <a:emoji_29:812376279814635591> m!uptime
-
-[SUPPORT](https://discord.gg/rWQh4SYQsR)    -    [INVITE](https://discord.com/api/oauth2/authorize?client_id=793214259932561419&permissions=8&scope=bot)
+ <a:emoji_15:818839370730569760> @loop 
+ <a:emoji_15:818839370730569760> @lyrics              
+ <a:emoji_15:818839370730569760> @pause
+ <a:emoji_15:818839370730569760> @play                   
+ <a:emoji_15:818839370730569760> @queue
+ <a:emoji_15:818839370730569760> @radio                 
+ <a:emoji_15:818839370730569760> @remove
+ <a:emoji_15:818839370730569760> @resume             
+ <a:emoji_15:818839370730569760> @search
+ <a:emoji_15:818839370730569760> @shuffle              
+ <a:emoji_15:818839370730569760> @skip
+ <a:emoji_15:818839370730569760> @skipto               
+ <a:emoji_15:818839370730569760> @stop
+ <a:emoji_15:818839370730569760> @volume
+◈ ━━━━━━━━ <a:setting:813404135181385759> ━━━━━━━━ ◈
+      <a:emoji_16:818842836890943488> Filter Commands <a:emoji_16:818842836890943488>
+<a:emoji_9:797912443628027955>  @fi 8D
+ <a:emoji_9:797912443628027955> @fi tremolo
+ <a:emoji_9:797912443628027955> @fi vibrato
+ <a:emoji_9:797912443628027955> @fi pulsator
+ <a:emoji_9:797912443628027955> @fi clear
+ <a:emoji_9:797912443628027955> @fi bassboost
+ <a:emoji_9:797912443628027955> @fi vaporwave
+ ━━━━━━━━ <a:emoji_16:818842836890943488> ━━━━━━━━ 
+      <a:discord:813406342173818910> Others <a:discord:813406342173818910> 
+<a:Erore:813505315534405632> @help              <a:remix:815540220031533087> @ping
+<a:Erore:813505315534405632> @prefix            <a:remix:815540220031533087> @uptime
+ ━━━━━━━━ <a:setting:813404135181385759> ━━━━━━━━ 
+[SUPPORT](https://discord.gg/PwZrRhmc3n)    -    [INVITE](https://discord.com/oauth2/authorize?client_id=817582387734904842&permissions=70282305&scope=bot)
 
 `)
 
   
-   .setColor("BLUE");
+   .setColor("RANDOM");
    helpEmbed.setTimestamp();
    message.react("<a:emoji_26:809385634149826611>")
     return message.channel.send(helpEmbed).catch(console.error);
